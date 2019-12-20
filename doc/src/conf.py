2ee983e525feee4a17f5105728bfc4d12b0ba357
Emmanuel Bertin <bertin@iap.fr>
2019-12-20 15:54:23 +0100

    Fixed make dist issue with doc subdirectory.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -80,7 +80,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Wed Dec 11 2019'
+today = 'Wed Dec 18 2019'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
