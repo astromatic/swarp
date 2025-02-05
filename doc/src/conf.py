1cd9f0b825e256843c3092dd920e42e0a4cea02c
Emmanuel Bertin <bertin@iap.fr>
2025-02-05 11:54:59 +0100

    Add .gitignore file.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -80,7 +80,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Wed Jan 27 2021'
+today = 'Wed Feb 05 2025'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
