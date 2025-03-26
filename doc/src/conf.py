7d46047453c7fffc75b9032b0b674ed123227d96
Emmanuel Bertin <bertin@iap.fr>
2025-03-26 15:24:01 +0100

    Use Intel's icx compiler with rpm-best option for RPM packages.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -80,7 +80,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Thu Mar 20 2025'
+today = 'Wed Mar 26 2025'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
